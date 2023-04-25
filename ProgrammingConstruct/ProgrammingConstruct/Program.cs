6dfa18d3e932d4c71c227c0a6eab6e928c0dd6b4
Krishanaku <krishnakumar.kk650.kk@gmail.com>
2023-04-25 11:38:30 +0530

    Add : 1.Write a C# Sharp program to accept two integers and check whether they are equal or not.

---
+++ ProgrammingConstruct/ProgrammingConstruct/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace ProgrammingConstruct
+{
+    public class Program
+    {
+        public static void Main(string[] args) 
+        {
+            //UC1......
+            ProgrammingConstruct programmingConstruct = new ProgrammingConstruct();
+            programmingConstruct.AcceptInteger();
+            Console.ReadKey();
+        }
+    }
+}
